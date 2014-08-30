eb9962b27b3e14537fdb622607bc29dfba2625d8
alacret <alacret@gmail.com>
2014-08-30 11:20:57 -0430

    test

--- udacity-web.py
+++ udacity-web.py
@@ -1,70 +1,21 @@
+import os
 import webapp2
 
-months = ['January','February','March','April','May','June',
-          'July','August','September','October','November','December']
+import jinja2
 
-dmontsh = dict((m[:3],m) for m in months)
-
-def valid_month(month):
-    if month:
-        month = month.capitalize()[:3]
-        return dmontsh.get(month)
-
-def valid_day(day):
-    try:
-        iday = int(day)
-    except Exception:
-        return
-    else:
-        return iday if (iday > 0 and iday <32) else None
-
-def valid_year(year):
-    try:
-        iyear = int(year)
-    except Exception:
-        return
-    else:
-        return iyear if (iyear > 1899 and iyear < 2021) else None
-
-form="""
-<form method="post">
-    What is your birthday?
-    <br>
-    <label> Month
-    <input name="month" value="%(month)s">
-    </label>
-    <label> Day
-    <input name="day">
-    </label>
-    <label> Year
-    <input name="year">
-    </label>
-    <div style="color:red">%(error)s</div>
-<input type="submit">
-</form>
-"""
+jinja_environment = jinja2.Environment(autoescape=True,
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
 
 class MainPage(webapp2.RequestHandler):
-    def write_form(self,error="",month="",day="",year=""):
-        self.response.out.write(form % {"error":error,
-                                        "month":month,
-                                        "day":day,
-                                        "year":year})
+    def get(self):
+        self.response.out.write("")
+
+class FizzBuzz(webapp2.RequestHandler):
     def get(self):
         self.write_form()
-    def post(self):
-        month = self.request.get('month')
-        user_month = valid_month(month)
-        day = self.request.get('day')
-        user_day = valid_month(day)
-        year = self.request.get('year')
-        user_year = valid_month(year)
 
-        if not (user_month and user_day and user_year):
-            self.write_form("Error",month,day,year)
-        else:
-            self.response.out.write("Thanks! That's a totally valid day!");
 
 application = webapp2.WSGIApplication([
 	('/', MainPage),
+    ('/fizzbuzz', FizzBuzz),
 ], debug=True)
