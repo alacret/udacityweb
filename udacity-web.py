ea31523d357770e95f6661cf4fd955fc8228125c
alacret <alacret@gmail.com>
2014-09-02 13:43:18 -0430

    lesson5

--- udacity-web.py
+++ udacity-web.py
@@ -1,14 +1,87 @@
-import os,json,jsonU
-import webapp2
+import os,json,jsonU, webapp2, re, jinja2, hashlib
 from google.appengine.ext import db
-import jinja2
 
 jinja = jinja2.Environment(autoescape=True,
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
 
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return USER_RE.match(username)
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_pass(val):
+    return PASS_RE.match(val)
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+def valid_email(val):
+    return EMAIL_RE.match(val)
+def duplicate_username(username):
+    r = User.all().filter("username =", username)
+    if r.count() > 0 :
+        return True
+    return False
+def valid_credentiales(username,password):
+    r = User.all().filter("username =", username).filter("password =", password)
+    if r.count() > 0 :
+        return True
+    return False
+
+sign_up_form="""
+<!doctype html>
+<html>
+<body>
+<h2>Signup</h2>
+    <form method="post">
+      <table>
+        <tr>
+          <td class="label">
+            Username
+          </td>
+          <td>
+            <input type="text" name="username" value="%(username)s">
+          </td>
+          <td class="error">%(username_error)s</td>
+        </tr>
+        <tr>
+          <td class="label">
+            Password
+          </td>
+          <td>
+            <input type="password" name="password" value="">
+          </td>
+          <td class="error">%(password_error)s</td>
+        </tr>
+        <tr>
+          <td class="label">
+            Verify Password
+          </td>
+          <td>
+            <input type="password" name="verify" value="">
+          </td>
+          <td class="error"></td>
+        </tr>
+
+        <tr>
+          <td class="label">
+            Email (optional)
+          </td>
+          <td>
+            <input type="text" name="email" value="%(email)s">
+          </td>
+          <td class="error">%(email_error)s</td>
+        </tr>
+      </table>
+      <input type="submit">
+    </form>
+</body>
+</html>
+"""
+
 def jinja_render(template, **params):
     t = jinja.get_template(template)
     return t.render(params)
+
+class User(db.Model):
+    username = db.StringProperty()
+    password = db.StringProperty()
 
 class Post(db.Model):
     subject = db.StringProperty(required =True)
@@ -61,10 +134,99 @@ class JSONPostHandler(webapp2.RequestHandler):
         self.response.headers["Content-type"]="application/json"
         self.response.out.write(jsonU.encode(post))
 
+class SignUpPage(webapp2.RequestHandler):
+    def write_form(self,username="",username_error="",password_error="",email="",email_error=""):
+        self.response.out.write(sign_up_form % {"username":username,
+                                                "username_error":username_error,
+                                                "password_error":password_error,
+                                                "email":email,
+                                                "email_error":email_error})
+    def get(self):
+        self.write_form()
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        username_error = ""
+        password_error = ""
+        email_error = ""
+
+        if not valid_username(username):
+            username_error = "Not a valid username"
+
+        if duplicate_username(username):
+            username_error = "Username already in database"
+
+        if password:
+            if not valid_pass(password):
+                password_error = "Not a valid password"
+            else:
+                if password != verify:
+                    password_error = "Password missmatch"
+        else:
+            password_error = "Must enter a password"
+
+        if email:
+            if not valid_email(email):
+                email_error = "Not a valid email"
+
+        if (username_error == "" and password_error == "" and email_error == ""):
+            user = User(username = username,password=password)
+            user.put()
+            digest = hashlib.md5(username).hexdigest()
+            cookie = str('username='+username+'|'+digest+'; Path=/')
+            self.response.headers.add_header('Set-Cookie', cookie)
+            self.redirect("/welcome")
+        else:
+            self.write_form(username, username_error, password_error, email, email_error)
+
+class WelcomePage(webapp2.RequestHandler):
+    def get(self):
+        username_cookie = self.request.cookies.get("username")
+        if not username_cookie:
+            self.redirect("/signup")
+        else:
+            t = username_cookie.split("|")
+            print username_cookie
+            if len(t) > 1:
+                hash = hashlib.md5(t[0]).hexdigest()
+                if hash == t[1]:
+                    self.response.out.write(jinja_render("thanks.html",username = t[0]))
+                else:
+                    self.redirect("/signup")
+            else:
+                self.redirect("/signup")
+
+class LoginPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write(jinja_render("login.html"))
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+
+        if username and password and valid_credentiales(username,password):
+            digest = hashlib.md5(username).hexdigest()
+            cookie = str('username='+username+'|'+digest+'; Path=/')
+            self.response.headers.add_header('Set-Cookie', cookie)
+            self.redirect("/welcome")
+        else:
+            self.response.out.write(jinja_render("login.html",error="Invalid login"))
+
+class LogoutPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.delete_cookie("username", path='/')
+        self.redirect("/signup")
+
 application = webapp2.WSGIApplication([
 	('/', PostsHandler),
     ('/.json', JSONPostsHandler),
     ('/newpost', NewPostHandler),
     (r'/post/(\d+)', PostHandler),
     (r'/post/(\d+).json', JSONPostHandler),
+    ('/signup', SignUpPage),
+    ('/welcome', WelcomePage),
+    ('/login', LoginPage),
+    ('/logout', LogoutPage),
 ], debug=True)
